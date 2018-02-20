92d177f5fd86c568c3af8785b04542735f26be96
Satyam Bansal <satyamsgsits1994@gmail.com>
2018-02-20 18:10:36 +0530

    error page

--- server/controllers/genreController.js
+++ server/controllers/genreController.js
@@ -1,11 +1,31 @@
-import Genre from '../models/Genre';
 
+import Genre from '../models/Genre';
+const {body,validationResult}=require('express-validator/check');
+const {sanitizeBody}=require('express-validator/filter');
 exports.genres=(req,res)=>{
-res.send("hi");
+Genre.find({},(err,result)=>{
+    if(err){
+        res.render('error',{"message":err.toString()});        
+    }
+    else{
+    res.render('genres',{genres:result});
+    }
+})
 }
 
 exports.genre_details=(req,res)=>{
 
+if(req.params.id){
+Genre.findById({_id:id},(err,result)=>{
+if(err){
+    res.render('error',{"message":err.toString()});
+}
+else{
+    console.log(result);
+    res.render('genre',{genre:result});
+}    
+})
+}
 }
 
 exports.create_genre_get=(req,res)=>{
